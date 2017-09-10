be348d358d7b1a76cec72f99422dc62f39f6deaa
Justyna Skalska <jastka4@gmail.com>
2017-09-10 22:05:59 +0200

    Service in profile page WIP

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProfilesService } from '../_services/profiles.service';
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
+  private _route: any;
+  private username: number;
+  profile: any = [];
+  profiles: any = [];
 
-  constructor() { }
+  constructor(private profilesService: ProfilesService,
+              private route: ActivatedRoute) {}
 
   ngOnInit() {
+    this.profiles = this.profilesService.profiles;
+    this._route = this.route.params.subscribe(params => {
+      this.username = params['username'];
+      this.profile = this.profiles.filter((profile)=>{
+        if(profile.username === this.username){
+          return profile
+        }
+      });
+      this.profile = this.profile[0];
+    });
+    console.log(this.profile)
   }
 
 }
